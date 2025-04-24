32dfc928ad2cacfba979e4b2638b9f8c7d189005
256-514 <dan.krouzil@gmail.com>
2025-04-24 23:01:32 +0200

    Update top_level.vhd

--- project_files/top_level.vhd
+++ project_files/top_level.vhd
@@ -4,42 +4,40 @@ use IEEE.NUMERIC_STD.ALL;
 
 entity top_level is
     Port ( 
-        CLK100MHZ : in STD_LOGIC;
-        SW       : in STD_LOGIC_VECTOR (8 downto 0);
-        JC0      : in STD_LOGIC;   -- Echo sensor 1
-        JA0      : out STD_LOGIC;   -- Trigger sensor 1               
-        JB0      : in STD_LOGIC;   -- Echo sensor 2
-        JD0      : out STD_LOGIC;   -- Trigger sensor 2     
-        BTNC     : in STD_LOGIC;   -- Reset
-        BTND     : in STD_LOGIC;   -- Show data button
-        BTNU     : in STD_LOGIC;   -- Show threshold button
-        LED      : out STD_LOGIC_VECTOR (15 downto 0);
-        CA       : out STD_LOGIC;
-        CB       : out STD_LOGIC;
-        CC       : out STD_LOGIC;
-        CD       : out STD_LOGIC;
-        CE       : out STD_LOGIC;
-        CF       : out STD_LOGIC;
-        CG       : out STD_LOGIC;
-        DP       : out STD_LOGIC;
-        AN       : out STD_LOGIC_VECTOR (7 downto 0)
+        CLK100MHZ : in  STD_LOGIC;
+        SW        : in  STD_LOGIC_VECTOR(8 downto 0);
+        JC0       : in  STD_LOGIC;   -- Left sensor echo
+        JA0       : out STD_LOGIC;   -- Left sensor trigger               
+        JB0       : in  STD_LOGIC;   -- Right sensor echo
+        JD0       : out STD_LOGIC;   -- Right sensor trigger     
+        BTNU      : in  STD_LOGIC;   -- Reset (changed from BTNC)
+        BTNC      : in  STD_LOGIC;   -- Show data button (changed from BTND)
+        BTND      : in  STD_LOGIC;   -- Show threshold button (changed from BTNU)
+        LED       : out STD_LOGIC_VECTOR(15 downto 0);
+        CA        : out STD_LOGIC;
+        CB        : out STD_LOGIC;
+        CC        : out STD_LOGIC;
+        CD        : out STD_LOGIC;
+        CE        : out STD_LOGIC;
+        CF        : out STD_LOGIC;
+        CG        : out STD_LOGIC;
+        DP        : out STD_LOGIC;
+        AN        : out STD_LOGIC_VECTOR(7 downto 0)
     );
 end top_level;
 
 architecture Behavioral of top_level is
     component echo_receiver is
         generic (
-            DEVICE_NUMBER : INTEGER := 1;
             MIN_DISTANCE : INTEGER := 50 
         );
         port ( 
-            trig        : in STD_LOGIC;
-            echo_in     : in STD_LOGIC;
-            clk        : in STD_LOGIC;
-            rst        : in STD_LOGIC;
-            dev_num    : out STD_LOGIC_VECTOR (2 downto 0);
-            distance   : out STD_LOGIC_VECTOR (8 downto 0);
-            status     : out STD_LOGIC 
+            trig      : in  STD_LOGIC;
+            echo_in   : in  STD_LOGIC;
+            clk       : in  STD_LOGIC;
+            rst       : in  STD_LOGIC;
+            distance  : out STD_LOGIC_VECTOR(8 downto 0);
+            status    : out STD_LOGIC 
         );
     end component;
     
@@ -59,165 +57,130 @@ architecture Behavioral of top_level is
     
     component trig_pulse is
         Port ( 
-            clk    : in STD_LOGIC;
-            start  : in STD_LOGIC;
+            clk    : in  STD_LOGIC;
+            start  : in  STD_LOGIC;
             trig   : out STD_LOGIC
         );
     end component;
     
     component display_control is
         Port (
-            clk           : in  std_logic;
-            reset         : in  std_logic;
-            distance1     : in  std_logic_vector(8 downto 0);
-            distance2     : in  std_logic_vector(8 downto 0);
-            threshold     : in  std_logic_vector(8 downto 0);
-            show_data_btn : in  std_logic;
-            show_thresh_btn: in std_logic;
-            seg           : out std_logic_vector(6 downto 0);
-            an            : out std_logic_vector(7 downto 0);
-            leds          : out std_logic_vector(15 downto 0)
-        );
-    end component;
-    
-    component clock_en is
-        generic (
-            n_periods : integer := 3
-        );
-        port (
-            clk   : in    std_logic;
-            rst   : in    std_logic;
-            pulse : out   std_logic
+            clk            : in  std_logic;
+            reset          : in  std_logic;
+            distance1      : in  std_logic_vector(8 downto 0);
+            distance2      : in  std_logic_vector(8 downto 0);
+            threshold      : in  std_logic_vector(8 downto 0);
+            show_data_btn  : in  std_logic;
+            show_thresh_btn: in  std_logic;
+            seg            : out std_logic_vector(6 downto 0);
+            an             : out std_logic_vector(7 downto 0);
+            leds           : out std_logic_vector(15 downto 0)
         );
     end component;
     
     signal reset : std_logic;
     
-    -- Sensor 1 signals
-    signal distance1_raw : std_logic_vector(8 downto 0);
-    signal distance1_processed : std_logic_vector(8 downto 0);
-    signal ready1, timeout1, valid1, thd1 : std_logic;
-    signal trigger1_start, trigger1_pulse : std_logic;
-    signal dev_num1 : std_logic_vector(2 downto 0);
-    signal status1 : std_logic;
+    -- Left sensor signals
+    signal left_distance_raw : std_logic_vector(8 downto 0);
+    signal left_distance_processed : std_logic_vector(8 downto 0);
+    signal left_ready, left_timeout, left_valid, left_thd : std_logic;
+    signal left_trigger_start, left_trigger_pulse : std_logic;
     
-    -- Sensor 2 signals
-    signal distance2_raw : std_logic_vector(8 downto 0);
-    signal distance2_processed : std_logic_vector(8 downto 0);
-    signal ready2, timeout2, valid2, thd2 : std_logic;
-    signal trigger2_start, trigger2_pulse : std_logic;
-    signal dev_num2 : std_logic_vector(2 downto 0);
-    signal status2 : std_logic;
+    -- Right sensor signals
+    signal right_distance_raw : std_logic_vector(8 downto 0);
+    signal right_distance_processed : std_logic_vector(8 downto 0);
+    signal right_ready, right_timeout, right_valid, right_thd : std_logic;
+    signal right_trigger_start, right_trigger_pulse : std_logic;
     
     -- Display signals
     signal seg_data : std_logic_vector(6 downto 0);
     signal anodes : std_logic_vector(7 downto 0);
     signal leds_internal : std_logic_vector(15 downto 0);
     
-    -- Clock enable for display refresh
-    signal display_refresh : std_logic;
-    
 begin
-    reset <= BTNC;
+    reset <= BTNU;
     
-    -- Display refresh clock enable
-    display_refresh_gen: clock_en
-        generic map (n_periods => 100000) -- 1kHz refresh
+    -- Left sensor processing
+    left_sensor: echo_receiver
+        generic map (MIN_DISTANCE => 50)
         port map (
-            clk => CLK100MHZ,
-            rst => reset,
-            pulse => display_refresh
-        );
-    
-    -- Sensor 1 components
-    echo_receiver_inst1: echo_receiver
-        generic map (
-            DEVICE_NUMBER => 1,
-            MIN_DISTANCE => 50
-        )
-        port map (
-            trig => trigger1_pulse,
+            trig    => left_trigger_pulse,
             echo_in => JC0,
-            clk => CLK100MHZ,
-            rst => reset,
-            dev_num => dev_num1,
-            distance => distance1_raw,
-            status => status1
+            clk     => CLK100MHZ,
+            rst     => reset,
+            distance => left_distance_raw,
+            status  => left_ready
         );
     
-    controller_inst1: controller
+    left_controller: controller
         port map (
-            clk => CLK100MHZ,
-            reset => reset,
-            distance_in => distance1_raw,
-            data_ready => status1,
-            timeout => '0',  -- Not used in current echo_receiver
-            trigger_out => trigger1_start,
-            distance_out => distance1_processed,
-            valid => valid1,
-            thd => thd1
+            clk         => CLK100MHZ,
+            reset       => reset,
+            distance_in => left_distance_raw,
+            data_ready  => left_ready,
+            timeout     => '0',
+            trigger_out => left_trigger_start,
+            distance_out => left_distance_processed,
+            valid       => left_valid,
+            thd         => left_thd
         );
     
-    trig_pulse_inst1: trig_pulse
+    left_trigger: trig_pulse
         port map (
-            clk => CLK100MHZ,
-            start => trigger1_start,
-            trig => trigger1_pulse
+            clk   => CLK100MHZ,
+            start => left_trigger_start,
+            trig  => left_trigger_pulse
         );
     
-    -- Sensor 2 components
-    echo_receiver_inst2: echo_receiver
-        generic map (
-            DEVICE_NUMBER => 2,
-            MIN_DISTANCE => 50
-        )
+    -- Right sensor processing
+    right_sensor: echo_receiver
+        generic map (MIN_DISTANCE => 50)
         port map (
-            trig => trigger2_pulse,
+            trig    => right_trigger_pulse,
             echo_in => JB0,
-            clk => CLK100MHZ,
-            rst => reset,
-            dev_num => dev_num2,
-            distance => distance2_raw,
-            status => status2
+            clk     => CLK100MHZ,
+            rst     => reset,
+            distance => right_distance_raw,
+            status  => right_ready
         );
     
-    controller_inst2: controller
+    right_controller: controller
         port map (
-            clk => CLK100MHZ,
-            reset => reset,
-            distance_in => distance2_raw,
-            data_ready => status2,
-            timeout => '0',  -- Not used in current echo_receiver
-            trigger_out => trigger2_start,
-            distance_out => distance2_processed,
-            valid => valid2,
-            thd => thd2
+            clk         => CLK100MHZ,
+            reset       => reset,
+            distance_in => right_distance_raw,
+            data_ready  => right_ready,
+            timeout     => '0',
+            trigger_out => right_trigger_start,
+            distance_out => right_distance_processed,
+            valid       => right_valid,
+            thd         => right_thd
         );
     
-    trig_pulse_inst2: trig_pulse
+    right_trigger: trig_pulse
         port map (
-            clk => CLK100MHZ,
-            start => trigger2_start,
-            trig => trigger2_pulse
+            clk   => CLK100MHZ,
+            start => right_trigger_start,
+            trig  => right_trigger_pulse
         );
     
-    -- Connect triggers to outputs
-    JA0 <= trigger1_pulse;
-    JD0 <= trigger2_pulse;
+    -- Connect triggers to physical pins
+    JA0 <= left_trigger_pulse;
+    JD0 <= right_trigger_pulse;
     
-    -- Display controller
+    -- Display system
     display: display_control
         port map (
-            clk => CLK100MHZ,
-            reset => reset,
-            distance1 => distance1_processed,
-            distance2 => distance2_processed,
-            threshold => SW,
-            show_data_btn => BTND,
-            show_thresh_btn => BTNU,
-            seg => seg_data,
-            an => anodes,
-            leds => leds_internal
+            clk            => CLK100MHZ,
+            reset          => reset,
+            distance1      => left_distance_processed,
+            distance2      => right_distance_processed,
+            threshold      => SW,
+            show_data_btn  => BTNC,
+            show_thresh_btn=> BTND,
+            seg            => seg_data,
+            an             => anodes,
+            leds           => leds_internal
         );
     
     -- Seven-segment display connections
@@ -228,7 +191,7 @@ begin
     CE <= seg_data(2);
     CF <= seg_data(1);
     CG <= seg_data(0);
-    DP <= '1';  -- Decimal point off
+    DP <= '1';  -- Decimal point always off
     AN <= anodes;
     
     -- LED connections
