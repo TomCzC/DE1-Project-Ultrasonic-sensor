ddd9b87277deb32ada1f417f4776204b65175b80
256-514 <dan.krouzil@gmail.com>
2025-04-24 14:14:01 +0200

    Update trig_pulse.vhd

--- project_files/trig_pulse.vhd
+++ project_files/trig_pulse.vhd
@@ -1,24 +1,17 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
 entity trig_pulse is
-    Port ( clk : in STD_LOGIC;
-           start : in STD_LOGIC;
-           trig1 : out STD_LOGIC;
-           trig2 : out STD_LOGIC);
+    Port ( 
+        clk   : in STD_LOGIC;
+        start : in STD_LOGIC;
+        trig  : out STD_LOGIC
+    );
 end trig_pulse;
 
 architecture Behavioral of trig_pulse is
-    signal counter      : integer range 0 to 10 := 0;
+    constant PULSE_WIDTH : integer := 1000; -- 10µs pulse at 100MHz clock
+    signal counter : integer range 0 to PULSE_WIDTH := 0;
     signal pulse_active : std_logic := '0';
 begin
     process(clk)
@@ -26,19 +19,16 @@ begin
         if rising_edge(clk) then
             if start = '1' and pulse_active = '0' then
                 pulse_active <= '1';
-                counter <= 1;
+                counter <= 0;
             elsif pulse_active = '1' then
-                if counter < 1 then
+                if counter < PULSE_WIDTH-1 then
                     counter <= counter + 1;
                 else
                     pulse_active <= '0';
-                    counter <= 0;
                 end if;
             end if;
         end if;
     end process;
 
-    trig1 <= pulse_active;
-    trig2 <= pulse_active;
-
+    trig <= pulse_active;
 end Behavioral;
